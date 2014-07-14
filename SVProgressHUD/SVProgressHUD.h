68c31a3eba46aa53bed3bb50800db26773569912
Ege Akpinar <egeakpinar@gmail.com>
2014-07-14 12:15:49 +0100

    Replaced with new SVProgressHUD library
    Which is cleaner and hopefully, easier on memory

--- SVProgressHUD/SVProgressHUD.h
+++ SVProgressHUD/SVProgressHUD.h
@@ -29,19 +29,16 @@ typedef NSUInteger SVProgressHUDMaskType;
 
 @interface SVProgressHUD : UIView
 
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 50000
-@property (readwrite, nonatomic, retain) UIColor *hudBackgroundColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIColor *hudForegroundColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIColor *hudStatusShadowColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIColor *hudRingBackgroundColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIColor *hudRingForegroundColor NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIFont *hudFont NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIImage *hudSuccessImage NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-@property (readwrite, nonatomic, retain) UIImage *hudErrorImage NS_AVAILABLE_IOS(5_0) UI_APPEARANCE_SELECTOR;
-#endif
+#pragma mark - Customization
 
-+ (void)setOffsetFromCenter:(UIOffset)offset;
-+ (void)resetOffsetFromCenter;
++ (void)setBackgroundColor:(UIColor*)color; // default is [UIColor whiteColor]
++ (void)setForegroundColor:(UIColor*)color; // default is [UIColor blackColor]
++ (void)setRingThickness:(CGFloat)width; // default is 4 pt
++ (void)setFont:(UIFont*)font; // default is [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline]
++ (void)setSuccessImage:(UIImage*)image; // default is bundled success image from Glyphish
++ (void)setErrorImage:(UIImage*)image; // default is bundled error image from Glyphish
+
+#pragma mark - Show Methods
 
 + (void)show;
 + (void)showWithMaskType:(SVProgressHUDMaskType)maskType;
@@ -55,20 +52,25 @@ typedef NSUInteger SVProgressHUDMaskType;
 + (void)setStatus:(NSString*)string; // change the HUD loading status while it's showing
 
 // stops the activity indicator, shows a glyph + status, and dismisses HUD 1s later
-+ (void)showSuccessWithStatus:(NSString *)string;
++ (void)showSuccessWithStatus:(NSString*)string;
 + (void)showErrorWithStatus:(NSString *)string;
-+ (void)showSuccessWithStatus:(NSString *)string duration:(float)duration;
-+ (void)showErrorWithStatus:(NSString *)string duration:(float)duration;
-/**
-  IMPORTANT NOTE- Callback is a static var which means it will get called for the first dismissed SVProgressHUD (not necessarily the caller)
- */ 
-+ (void)showSuccessWithStatus:(NSString *)string duration:(float)duration completion:(void (^)(void))callback;
-+ (void)showErrorWithStatus:(NSString *)string duration:(float)duration completion:(void (^)(void))callback;
 + (void)showImage:(UIImage*)image status:(NSString*)status; // use 28x28 white pngs
+
++ (void)setOffsetFromCenter:(UIOffset)offset;
++ (void)resetOffsetFromCenter;
 
 + (void)popActivity;
 + (void)dismiss;
 
 + (BOOL)isVisible;
+
+@end
+
+
+@interface SVIndefiniteAnimatedView : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
 
 @end
